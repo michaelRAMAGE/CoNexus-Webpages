ea42862514a100c9f421d6927bb12561c0903507
michael <mikeramage.ed@gmail.com>
2024-01-04 06:39:54 -0500

    fixed hover issue

--- src/components/AnimateTile.svelte
+++ src/components/AnimateTile.svelte
@@ -1,24 +1,18 @@
 <script>
-  import { createEventDispatcher } from 'svelte';
-
   export let primaryImage = "";
   export let secondaryImage = "";
   export let alt = "";
   export let id = "";
-  export let isLink = false;
-  export let styles = ""; 
+  export let styles = "";
 
-  let isHovered = false; 
+  let isHovered = false;
 </script>
 
-{#if isLink}
-  <a href={id} id={id} class='block' >
-    <img src={isHovered ? secondaryImage : primaryImage} alt={alt} class={`${styles}`} on:mouseover={() => isHovered = true} on:mouseout={() => isHovered = false}/>
-  </a>
-{:else}
-  <div id={id} on:click={handleClick} class='cursor-pointer' on:mouseover={() => isHovered = true} on:mouseout={() => isHovered = false}>
-    <img src={isHovered ? secondaryImage : primaryImage} alt={alt} class={`${styles}`} />
-  </div>
-{/if}
-
-
+<div
+  id={id}
+  on:mouseover={() => isHovered = true}
+  on:mouseout={() => isHovered = false}
+  class='cursor-pointer'
+>
+  <img src={isHovered ? secondaryImage : primaryImage} alt={alt} class={`${styles}`} />
+</div>
--- src/components/TileCollection.svelte
+++ src/components/TileCollection.svelte
@@ -10,13 +10,24 @@
 
 <div class={`${collectionStyles}`}>
   {#each tileData as story (story.name)}
-    <AnimateTile
-      id={story.name}
-      primaryImage={story.thumbnail_primary}
-      secondaryImage={story.thumbnail_secondary}
-      alt={story.name}
-      styles={tileStyles}
-      isLink={areLinks ? true : false}
-    />
+    {#if areLinks}
+      <a href={story.name} id={story.name} class='block' >
+        <AnimateTile
+          id={story.name}
+          primaryImage={story.thumbnail_primary}
+          secondaryImage={story.thumbnail_secondary}
+          alt={story.name}
+          styles={tileStyles}
+        />
+      </a>
+    {:else} 
+      <AnimateTile
+        id={story.name}
+        primaryImage={story.thumbnail_primary}
+        secondaryImage={story.thumbnail_secondary}
+        alt={story.name}
+        styles={tileStyles}
+      />
+    {/if}
   {/each}
 </div>
